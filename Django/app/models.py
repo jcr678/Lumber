6da6786fe3f0fa50614db2881d95e9c82e1063c3
jacob charles richard <jacobcrichard8@gmail.com>
2020-04-06 21:46:59 -0500

    sqlite

--- Django/app/models.py
+++ Django/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. LATER THIS IS THE DATABASE
+class Question(models.Model):
+    #column name=datatype
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    #each choice is associated with 1 question.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
