cd19fd1e0381720ce5f1b7e487077622e6f011d7
jacob charles richard <jacobcrichard8@gmail.com>
2020-04-07 15:56:30 -0500

    THIS IS THE DATABASE MODEL

--- Django/app/models.py
+++ Django/app/models.py
@@ -2,21 +2,32 @@ from django.db import models
 from django.utils import timezone
 import datetime
 # Create your models here. LATER THIS IS THE DATABASE
-class Question(models.Model):
+class Person(models.Model):
     #column name=datatype
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    isemployee = models.BooleanField(default=False)
+    startemploymentweeks = models.IntegerField()
+    endemploymentweeks = models.IntegerField()
+    matches =
+    messages=models.TextField()
+    profile_pic=
+    Jobtype=models.TextField()
+    swipesinterestedinme=models.ForeignKey(Person, on_delete=models.CASCADE)
+    swipesiminterestedin=models.ForeignKey(Person, on_delete=models.CASCADE)
+    availabilemonday=models.BooleanField(default=False)
+    availabiletuesday=models.BooleanField(default=False)
+    availabilewednesay=models.BooleanField(default=False)
+    availabilethursday=models.BooleanField(default=False)
+    availabilefriday=models.BooleanField(default=False)
+    availabilesaturday=models.BooleanField(default=False)
+    availabilesunday=models.BooleanField(default=False)
+    resume=
+    coverpage=
+    introduction=models.TextField()
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     #change value displayed for Question.objects.all()
     def __str__(self):
-        return self.question_text
-
-class Choice(models.Model):
-    #each choice is associated with 1 question.
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return self.question_text +
