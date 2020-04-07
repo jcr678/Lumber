eee81695b571a7e8ba58a9f403b37c56d20711f2
Anthony-Tien N Huynh <34702227+Ant1997@users.noreply.github.com>
2020-04-07 17:03:20 -0500

    SignUp Page
    http://127.0.0.1:8000/signup/
    
    Need to save user information.

--- BackEndDirectory/BackEnd/settings.py
+++ BackEndDirectory/BackEnd/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'userLogin.apps.UserloginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
