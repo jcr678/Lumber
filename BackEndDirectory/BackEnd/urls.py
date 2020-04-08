0f56101ddb8d7cb9691bcad20063f886c83462d4
Anthony-Tien N Huynh <34702227+Ant1997@users.noreply.github.com>
2020-04-07 21:25:36 -0500

    Fixed Images on Sign Up Page
    In this commit, the sign up page has been fixed. Images loaded properly. Need to save user's info in database next.

--- BackEndDirectory/BackEnd/urls.py
+++ BackEndDirectory/BackEnd/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     #url(r'^$', include('userLogin.urls')),
     url(r'^signup/', include('userLogin.urls')),
-]
+
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- BackEndDirectory/userLogin/urls.py
+++ BackEndDirectory/userLogin/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from . import views
 
 urlpatterns = [
    url(r'^$', views.UserFormView.as_view(), name='register'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
